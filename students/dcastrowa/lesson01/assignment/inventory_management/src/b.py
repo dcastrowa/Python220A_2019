5720172315314c4dc78ff04fb2c5ad3138cfa10f
Daniel Castro <daniel.castro@critigen.com>
2019-04-07 12:33:44 -0700

    Clear b.py and c.py

---
+++ students/dcastrowa/lesson01/assignment/inventory_management/src/b.py
@@ -0,0 +1,12 @@
+'''
+b.py in students/dcastrowa/lessons/lesson01/assignments/src
+run me from  students/dcastrowa/lessons/lesson01/assignments/src
+'''
+
+def print_mess(message):
+    '''
+    prints a message
+    :param message: a string message
+    :return: print message
+    '''
+    print(message)
---
+++ students/dcastrowa/lesson01/assignment/inventory_management/src/c.py
@@ -0,0 +1,22 @@
+'''
+I am a.py in students/dcastrowa/lessons/lesson01/assignments/src
+run me from  students/dcastrowa/lessons/lesson01/assignments/src
+
+'''
+
+
+def show_mess(message):
+    '''
+    Enter a messsage to be printed
+    :param message: a string message
+    :return: your message
+    '''
+    print(message)
+
+
+def missed():
+    '''
+    Prints the number 0
+    :return: prints 0
+    '''
+    print(0)
