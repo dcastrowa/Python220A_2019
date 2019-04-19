c6fde615f75d83e85fe3f58c56082307565b09b1
Daniel Castro <daniel.castro@critigen.com>
2019-04-18 20:32:10 -0700

    Create customer model

---
+++ students/dcastrowa/lesson03/assignment/src/basic_operations.py
@@ -0,0 +1,7 @@
+'''
+basic_operations.py
+Operation class to create, remove, update, and delete customer data
+'''
+
+from peewee import *
+from customer_model
---
+++ students/dcastrowa/lesson03/assignment/src/customer_model.py
@@ -0,0 +1,41 @@
+import logging
+from peewee import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info('Here we define our data (the schema)')
+logger.info('First name and connect to a database (sqlite here)')
+
+logger.info('The next 3 lines of code are the only database specific code')
+
+database = SqliteDatabase('lesson03.db')
+database.connect()
+database.execute_sql('PRAGMA foreign_keys = ON;')
+logger.info('Connect to database')
+
+
+# Build BaseModel
+class BaseModel(Model):
+    '''
+    Base model from peewee
+    '''
+    class Meta:
+        database = database
+
+
+# Customer class to hold all customer data
+class Customer(BaseModel):
+    '''
+    This class is the Customer class that stores all data about
+    our customers.
+    '''
+    customer_id = IntegerField(primary_key=True)
+    first_name = CharField(max_length=30)
+    last_name = CharField(max_length=30)
+    home_address = CharField(max_length=80)
+    phone_number = DecimalField(max_digits=10)
+    email_address = CharField(max_length=80)
+    status = BooleanField
+    credit_limit = IntegerField(null=False)
+
